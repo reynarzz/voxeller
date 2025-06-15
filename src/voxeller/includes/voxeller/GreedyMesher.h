7fc832a7277e30cda343abd1e80343c4484fb25b
Reynardo Perez <reynardohiraldo@hotmail.com>
2025-06-15 18:51:12 -0400

    improvements to description

--- src/voxeller/includes/voxeller/GreedyMesher.h
+++ src/voxeller/includes/voxeller/GreedyMesher.h
@@ -41,15 +41,15 @@ enum class TextureFormat
 enum class TextureType
 {
     // Will generate a performant mesh, but texture will be more complex and bigger in size. 
-    // Perfect for complex meshes.
+    // Perfect for complex meshes. Bake supported.
     Atlas,
 
     // Will generate a not so performant mesh, and a no so performant texture, but final mesh fidelity will be higher. 
-    // Great for non interactive media (videos/images).
+    // Great for non interactive media (videos/images). Bake supported.
     AtlasHighFidelity,
 
     // Will generate a mesh with more complex topology but simpler and smaller texture made of individual colors per pixel. 
-    // Perfect for simple meshes.
+    // Perfect for simple meshes. Baking is not possible/harder to do.
     Palette
 };
 
